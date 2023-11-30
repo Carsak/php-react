af10e11aecf63a1d96a74d2db0c0c629ddaeb666
Carsak <1649207-Carsak@users.noreply.gitlab.com>
2023-11-30 15:27:14 +0600

    created dummy file for async test

--- create-connections-async.php
+++ create-connections-async.php
@@ -11,35 +11,32 @@ $db_name = 'bitrix_local';
 $pdo = new PDO("mysql:host=$host;dbname=$db_name", $user , $pass);
 
 for ($month = 1; $month <= 12; $month++) {
-//    $current_directory = __DIR__ . '/' . $month;
-//
-//    if (!is_dir($current_directory)) {
-//        mkdir($current_directory);
-//    }
+    $open_start = microtime(true);
 
     $pdo = new PDO("mysql:host=$host;dbname=$db_name", $user , $pass);
+
+    $open_ended = microtime(true);
+
+    writeToLog('Opening connection', $open_start, $open_ended);
+
     for ($i = 0; $i < 300; $i++) {
         $time = microtime(true);
         $result = $pdo->query("SELECT SQL_NO_CACHE $time");
         $result->fetch();
-
-
-//        file_put_contents("$current_directory/$month-$time.txt", print_r(['month' => $month, 'time' => $time], true), FILE_APPEND);
     }
     unset($pdo);
 }
 
 $finish = microtime(true);
-$diff = $finish - $start_time;
 
-writeToLog($diff);
+writeToLog('Script ended', $start_time, $finish);
 
-function writeToLog(float $time): void
+function writeToLog(string $message, float $start, float $finish): void
 {
+    $diff = $finish - $start;
     $log = "\n------------------------\n";
     $log .= date("Y.m.d H:i:s") . "\n";
-//            $log .= (strlen($title) > 0 ? $title : 'DEBUG') . "\n";
-    $log .= print_r(['message' => 'Script ended', 'time' => $time], true);
+    $log .= print_r(['message' => $message, 'start' => $start, 'finish' => $finish, 'total' => $diff], true);
     $log .= "\n------------------------\n";
     file_put_contents(__DIR__ . "/log.log", $log, FILE_APPEND);
 }
